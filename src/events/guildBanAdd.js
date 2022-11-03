8b02edc7dd1476ff936b70978963fee629797bc2
defnot001 <marc.miller994@gmail.com>
2022-11-03 21:54:55 +0100

    made this work

--- src/events/guildBanAdd.js
+++ src/events/guildBanAdd.js
@@ -3,41 +3,36 @@ import guildconfig from '../config/guildConfig.js';
 
 export const event = {
   name: 'guildBanAdd',
-  async execute(ban) {
-    console.log(`${ban.user.tag} was banned from ${ban.guild.name}.`);
+  async execute(bannedMember) {
+    console.log(
+      `${bannedMember.user.tag} was banned from ${bannedMember.guild.name}.`,
+    );
 
     const fetchedLogs = await ban.guild.fetchAuditLogs({
       limit: 1,
       type: AuditLogEvent.MemberBanAdd,
     });
 
-    const modLogChannel = ban.guild.channels.cache.get(
-      guildconfig.modLogChannelId,
-    );
     const banLog = fetchedLogs.entries.first();
-
     if (!banLog) return;
 
     const { executor, target } = banLog;
+    const reason = banLog.reason || 'No reason provided';
+    const executingMember = await bannedMember.guild.members.fetch(executor.id);
 
-    if (target.id === ban.user.id) {
-      modLogChannel.send(
-        `${inlineCode(ban.user.tag)} (<@${
-          ban.user.id
-        }>) got hit with the swift hammer of justice in the guild ${inlineCode(
-          ban.guild.name,
-        )}, wielded by the mighty ${inlineCode(executor.tag)} (<@${
-          executor.id
-        }>)!`,
+    if (target.id === bannedMember.id) {
+      console.log(
+        `${bannedMember.user.tag} was banned from ${bannedMember.guild.name}.`,
       );
-    } else {
-      modLogChannel.send(
-        `${inlineCode(ban.user.tag)} (<@${
-          ban.user.id
-        }>) got hit with the swift hammer of justice in the guild ${inlineCode(
-          ban.guild.name,
-        )}, audit log fetch was inconclusive.`,
+
+      const banEmbed = buildModerationEmbed(
+        bannedMember,
+        'Ban',
+        reason,
+        executingMember,
       );
+
+      modLog.send({ embeds: [banEmbed] });
     }
   },
 };
