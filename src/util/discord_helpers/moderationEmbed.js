c5d4e33b527a5ddd3bf07a3a594709fa69f5e250
defnot001 <marc.miller994@gmail.com>
2022-11-04 13:40:15 +0100

    major cleanup

--- src/util/discord_helpers/moderationEmbed.js
+++ src/util/discord_helpers/moderationEmbed.js
@@ -7,34 +7,48 @@ const buildModerationEmbed = (
   executor,
   expiration = null,
 ) => {
-  const description = {
+  const descriptionObject = {
     member: `**Member**: ${member.user.tag} (${inlineCode(member.user.id)})`,
     action: `**Action**: ${action}`,
     reason: `**Reason**: ${reason}`,
     expiration: `**Expiration**: ${expiration}`,
   };
 
+  if (!expiration) {
+    delete descriptionObject.expiration;
+  }
+
+  const description = Object.values(descriptionObject).join('\n');
+
   const moderationEmbed = new EmbedBuilder({
     author: {
       name: executor.user.tag,
       icon_url: executor.user.displayAvatarURL(),
     },
+    description: description,
     footer: { text: 'Moderation Logger' },
     timestamp: Date.now(),
   });
 
-  if (action === 'Kick' || action === 'Ban') {
-    moderationEmbed
-      .setColor('Orange')
-      .setDescription(
-        `${description.member}\n${description.action}\n${description.reason}`,
-      );
-  } else if (action === 'Timeout') {
-    moderationEmbed
-      .setColor('Yellow')
-      .setDescription(
-        `${description.member}\n${description.action}\n${description.reason}\n${description.expiration}`,
-      );
+  switch (action) {
+    case 'Kick':
+      moderationEmbed.setColor('Orange');
+      break;
+    case 'Ban':
+      moderationEmbed.setColor('Red');
+      break;
+    case 'Unban':
+      moderationEmbed.setColor(3_092_790);
+      break;
+    case 'Timeout':
+      moderationEmbed.setColor('Yellow');
+      break;
+    case 'Remove Timeout':
+      moderationEmbed.setColor(3_092_790);
+      break;
+
+    default:
+      moderationEmbed.setColor(3_092_790);
   }
 
   return moderationEmbed;
