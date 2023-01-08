664d4fb2722953bd7316ac5788ef200a1c5920a2
defnot001 <marc.miller994@gmail.com>
2023-01-08 20:12:43 +0100

    added avatar options and changed config

--- src/typings/types/typeHelpers.ts
+++ src/typings/types/typeHelpers.ts
@@ -1,4 +1,4 @@
-import type { config } from '../../config/config';
+import type { preconfig } from '../../config/config';
 
 export type TPowerAction = 'start' | 'stop' | 'restart' | 'kill';
 export type TPowerActionNoStart = Omit<TPowerAction, 'start'>;
@@ -9,8 +9,70 @@ export type MCServerSubcommand =
   | 'kill'
   | 'stats';
 
-export type TServerChoice = keyof typeof config.mcConfig;
-export type TConfig = typeof config;
+export type TServerChoice = keyof typeof preconfig.mcConfig;
 export type TConfigKeys = keyof TConfig;
 export type TChannelName = keyof TConfig['channels'];
 export type TModerationAction = 'kick' | 'ban' | 'unban';
+export type TAvatarOptions =
+  | '/avatars/'
+  | '/renders/head/'
+  | '/renders/body/'
+  | '/skins/';
+export type TMojangUUIDResponse = {
+  name: string;
+  id: string;
+};
+export type TConfig = {
+  bot: {
+    readonly token: string;
+    readonly clientID: string;
+    readonly guildID: string;
+  };
+  ptero: {
+    readonly url: string;
+    readonly apiKey: string;
+  };
+  channels: {
+    readonly memberLog: string;
+    readonly modLog: string;
+    readonly botLog: string;
+    readonly invite: string;
+    readonly resources: string;
+    readonly serverInfo: string;
+    readonly todo: string;
+  };
+  roles: {
+    readonly members: string;
+    readonly admins: string;
+  };
+  embedColors: {
+    readonly default: number;
+    readonly none: number;
+    readonly red: number;
+    readonly orange: number;
+    readonly yellow: number;
+    readonly green: number;
+  };
+  emoji: {
+    readonly kiwi: string;
+    readonly owoKiwi: string;
+    readonly froghypers: string;
+  };
+  mcConfig: {
+    readonly smp: TMCServerConfig;
+    readonly cmp: TMCServerConfig;
+    readonly cmp2: TMCServerConfig;
+    readonly copy: TMCServerConfig;
+    readonly snapshots: TMCServerConfig;
+  };
+};
+
+export type TMCServerConfig = {
+  readonly host: string;
+  readonly port: number;
+  readonly serverId: string;
+  readonly rconPort: number;
+  readonly rconPasswd: string;
+  readonly operator: boolean;
+  readonly backupLimit: number;
+};
