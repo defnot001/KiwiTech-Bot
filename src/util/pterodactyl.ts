b88a5e572a9d8c2709d5538356118f87c5a05f28
defnot001 <marc.miller994@gmail.com>
2023-02-20 23:06:37 +0100

    added function to get mods from a server

--- src/util/pterodactyl.ts
+++ src/util/pterodactyl.ts
@@ -1,7 +1,18 @@
 import { PteroClient } from 'ptero-client';
 import { config } from '../config/config';
+import type { TServerChoice } from '../types/minecraft';
 
 export const ptero = new PteroClient({
   baseURL: config.ptero.url,
   apiKey: config.ptero.apiKey,
 });
+
+export async function getModFiles(serverChoice: TServerChoice) {
+  const modFiles = await (
+    await ptero.files.list(config.mcConfig[serverChoice].serverId, '/mods')
+  ).filter((mod) => {
+    return mod.is_file;
+  });
+
+  return modFiles;
+}
