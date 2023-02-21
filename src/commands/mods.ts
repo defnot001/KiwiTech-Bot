ba053d088f8b23a0d116b531f441e7819ac4e2a6
defnot001 <marc.miller994@gmail.com>
2023-02-21 12:41:28 +0100

    small changes of control flow

--- src/commands/mods.ts
+++ src/commands/mods.ts
@@ -2,10 +2,11 @@ import { ApplicationCommandOptionType } from 'discord.js';
 import { Command } from 'djs-handlers';
 import { KoalaEmbedBuilder } from '../classes/KoalaEmbedBuilder';
 import { config } from '../config/config';
+import type { TServerChoice } from '../types/minecraft';
 import { mcServerChoice } from '../util/components';
 import getErrorMessage from '../util/errors';
 import { createInteractionErrorLog } from '../util/loggers';
-import { getModFiles, ptero } from '../util/pterodactyl';
+import { getModFiles, getModNames, ptero } from '../util/pterodactyl';
 
 const modnameOption = {
   name: 'modname',
@@ -42,51 +43,30 @@ export default new Command({
     await interaction.deferReply();
 
     const subcommand = args.getSubcommand();
-    const serverChoice = args.getString(
-      'server',
-    ) as keyof typeof config.mcConfig;
-
-    if (!serverChoice) {
-      return interaction.editReply('Cannot find server!');
-    }
-
-    const serverID = config.mcConfig[serverChoice].serverId;
     const interactionGuild = interaction.guild;
 
     if (!interactionGuild) {
       return interaction.editReply('Cannot find guild!');
     }
 
+    const serverChoice = args.getString('server') as TServerChoice;
+
+    if (!serverChoice) {
+      return interaction.editReply('Cannot find server!');
+    }
+
     try {
-      const modFiles = await getModFiles(serverChoice);
-
-      if (modFiles.length === 0) {
-        return interaction.editReply(
-          'Cannot find any mods in the mods folder!',
-        );
-      }
-
-      const mods = {
-        enabled: modFiles.filter((mod) => {
-          return mod.name.endsWith('.jar');
-        }),
-        disabled: modFiles.filter((mod) => {
-          return mod.name.endsWith('.disabled');
-        }),
-      };
-
       if (subcommand === 'list') {
-        const modNames = {
-          enabled: mods.enabled.map((mod) => {
-            return mod.name.replace('.jar', '');
-          }),
-          disabled: mods.disabled.map((mod) => {
-            return mod.name.replace('.disabled', '');
-          }),
-        };
+        const modNames = await getModNames(serverChoice);
+
+        if (modNames.enabled.length === 0 && modNames.disabled.length === 0) {
+          return interaction.editReply(
+            'Cannot find any mods in the mods folder!',
+          );
+        }
 
         const modListEmbed = new KoalaEmbedBuilder(interaction.user, {
-          title: `Mod List for ${interactionGuild.name} ${serverChoice}`,
+          title: `Modlist for ${interactionGuild.name} ${serverChoice}`,
           fields: [
             {
               name: 'Enabled Mods',
@@ -107,14 +87,15 @@ export default new Command({
         const modname = args.getString('modname');
 
         if (!modname) {
-          return interaction.editReply('Cannot find mod name!');
+          return interaction.editReply('Cannot find modname!');
         }
 
-        const targetMods = modFiles.filter((mod) => {
-          return (
-            mod.name === `${modname}.jar` || mod.name === `${modname}.disabled`
-          );
-        });
+        const modFiles = await getModFiles(serverChoice);
+
+        const targetMods = modFiles.filter(
+          (mod) =>
+            mod.name === `${modname}.jar` || mod.name === `${modname}.disabled`,
+        );
 
         if (targetMods.length === 0 || !targetMods[0]) {
           return interaction.editReply(`Cannot find mod: ${modname}!`);
@@ -136,7 +117,7 @@ export default new Command({
               );
             }
 
-            await ptero.files.rename(serverID, {
+            await ptero.files.rename(config.mcConfig[serverChoice].serverId, {
               from: targetMod.name,
               to: targetMod.name.replace('.disabled', '.jar'),
               directory: '/mods',
@@ -155,7 +136,7 @@ export default new Command({
               );
             }
 
-            await ptero.files.rename(serverID, {
+            await ptero.files.rename(config.mcConfig[serverChoice].serverId, {
               from: targetMod.name,
               to: targetMod.name.replace('.jar', '.disabled'),
               directory: '/mods',
