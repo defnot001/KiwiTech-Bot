c243dc2ff63dfcc0dca3a365b989c43343d40b63
defnot001 <marc.miller994@gmail.com>
2023-03-17 08:42:21 +0100

    changed db to postgresql and added model

--- prisma/migrations/20230224173826_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "createdBy" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
