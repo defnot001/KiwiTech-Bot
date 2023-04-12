f53356186846fdbdc84b774bc55a61a769a46cac
defnot001 <marc.miller994@gmail.com>
2023-04-12 21:56:15 +0200

    moved associated function into the file

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,7 +1,7 @@
 import { inlineCode, time, userMention } from 'discord.js';
 import { Event } from 'djs-handlers';
 import { JoinLeaveEmbedBuilder } from '../classes/JoinLeaveEmbedBuilder';
-import { colorFromDuration, getJoinedAtComponent } from '../util/helpers';
+import { getJoinedAtComponent } from '../util/helpers';
 import { getTextChannelFromID, handleEventError } from '../util/loggers';
 export default new Event('guildMemberAdd', async (member) => {
   try {
@@ -34,3 +34,27 @@ export default new Event('guildMemberAdd', async (member) => {
     });
   }
 });
+
+function colorFromDuration(duration: number): number {
+  const MAX_TRUST_ACCOUNT_AGE = 1_000 * 60 * 60 * 24 * 7 * 4;
+  const percent = Math.min(duration / (MAX_TRUST_ACCOUNT_AGE / 100), 100);
+  let red;
+  let green;
+  let blue = 0;
+
+  if (percent < 50) {
+    red = 255;
+    green = Math.round(5.1 * percent);
+  } else {
+    green = 255;
+    red = Math.round(510 - 5.1 * percent);
+  }
+
+  const tintFactor = 0.3;
+
+  red += (255 - red) * tintFactor;
+  green += (255 - green) * tintFactor;
+  blue += (255 - blue) * tintFactor;
+
+  return Math.floor((red << 16) + (green << 8) + blue);
+}
