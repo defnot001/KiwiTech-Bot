15bd05280634f036bf8650b79cceb612a63f5dc9
defnot001 <marc.miller994@gmail.com>
2023-08-30 10:26:15 +0200

    added . at the end of the log

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,6 +5,6 @@ import { Event } from 'djs-handlers';
 
 export default new Event('ready', async (c) => {
   c.user.setActivity('Commands', { type: ActivityType.Listening });
-  console.log(`Bot is ready! Logged in as ${c.user.username}`);
+  console.log(`Bot is ready! Logged in as ${c.user.username}.`);
   // await client.removeCommands(config.bot.guildID);
 });
